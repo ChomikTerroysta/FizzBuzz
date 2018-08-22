59918e65038effd146c94aef93726014f34c2124
Marcin Wyrwalski <wyrwalm@gmai.com>
2018-08-22 20:29:44 +0200

    Working

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        for (int i=1; i<=150; i++){
+            if (i%3 ==0 && i%5==0){
+                System.out.println("FizzBuzz "+i);
+            }
+            else if (i%3==0){
+                System.out.println("Fizz "+i);
+            }
+            else if (i%5==0){
+                System.out.println("Buzz"+i);
+            }
+        }
+    }
+}
